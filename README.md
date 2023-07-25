afbea85856936a4e82d25ba096c2662e2a1dd244
lionel-rowe <lionel.rowe@gmail.com>
2023-07-25 14:11:12 +0800

    feat: Human-friendly date display

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. Clone this repo to your own GitHub account
 2. Set up GitHub pages
 3. View your activity at `https://<YOUR_GITHUB_USERNAME>.github.io/activity/`
+
+You can view another user’s activity instead by appending the query param `?user=<OTHER_GITHUB_USERNAME>`.
--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+async function initDateFormats() {
+	try {
+		const { default: dayjs } = await import(
+			'https://cdn.jsdelivr.net/npm/dayjs@1.11.9/+esm'
+		)
+		const { default: relativeTime } = await import(
+			'https://cdn.jsdelivr.net/npm/dayjs@1.11.9/plugin/relativeTime/+esm'
+		)
+
+		dayjs.extend(relativeTime)
+
+		globalThis.dayjs = dayjs
+
+		return (ts) => {
+			const d = dayjs(ts)
+
+			const diff = Math.abs(d.diff(dayjs(), 'days'))
+			const rel = d.fromNow()
+
+			const full = d.format('YYYY-MM-DD [at] hh:mm:ss (Z)')
+
+			const pretty = diff < 25 ? rel : d.format('MMM DD, YYYY')
+
+			return { full, pretty }
+		}
+	} catch {
+		return (ts) => {
+			const d = new Date(ts)
+
+			return {
+				full: d.toISOString(),
+				pretty: d.toLocaleString(),
+			}
+		}
+	}
+}
+
 const url = new URL(location.href)
 const qps = url.searchParams
 
@@ -5,58 +42,87 @@ const username = qps.has('user')
 	? qps.get('user')
 	: new URL(location.href).hostname.split('.')[0]
 
-const activities = await (
-	await fetch(
-		`https://api.github.com/users/${encodeURIComponent(
-			username,
-		)}/events?per_page=100`,
-	)
-).json()
+const [dateFormats, activities] = await Promise.all([
+	initDateFormats(),
+	(
+		await fetch(
+			`https://api.github.com/users/${encodeURIComponent(
+				username,
+			)}/events?per_page=100`,
+		)
+	).json(),
+])
 
-function escape(str) {
-	str = typeof str === 'string' ? str : JSON.stringify(str ?? null)
+class Html {
+	constructor(input, { trusted } = { trusted: false }) {
+		if (input instanceof this.constructor) {
+			return input
+		}
 
-	return str.replaceAll(/[&<>"']/g, (x) => `&#${x.codePointAt(0)};`)
+		const str = String(input)
+		const escaped = trusted
+			? str
+			: str.replaceAll(/[&<>"']/g, (x) => `&#${x.codePointAt(0)};`)
+
+		this.html = escaped
+	}
+
+	toString() {
+		return this.html
+	}
+}
+
+function escapeHtml(input) {
+	return Array.isArray(input)
+		? new Html(input.map(escapeHtml).join(''), { trusted: true })
+		: new Html(input)
+}
+
+function h({ raw }, ...args) {
+	return new Html(String.raw({ raw }, ...args.map(escapeHtml)), {
+		trusted: true,
+	})
 }
 
 function capitalize(str) {
-	return str.charAt(0).toUpperCase() + str.slice(1)
+	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+}
+
+function sentenceCase(str) {
+	return capitalize(str.replaceAll(/(?<=\p{Ll})(?=\p{Lu})/gu, ' '))
 }
 
 function link({ url, text, class: className }) {
-	return `<a target="_blank" href="${escape(url)}"${
-		className ? `class="${className}"` : ''
-	}>${escape(text)}</a>`
+	return h`<a href="${url}"${
+		className ? h`class="${className}"` : ''
+	}>${text}</a>`
 }
 
 function repoLink(x, { mainLink } = {}) {
-	return `${link({
+	if (!x.repo) return 'unknown repo'
+
+	return link({
 		url: x.repo.url.replace(
 			'https://api.github.com/repos/',
 			'https://github.com/',
 		),
 		text: x.repo.name,
 		class: mainLink ? '' : 'deemphasized',
-	})}`
+	})
 }
 
 function ts(x) {
-	let d = new Date(x.created_at)
-	d = new Date(d.valueOf() - d.getTimezoneOffset() * 60 * 1000)
+	const { full, pretty } = dateFormats(x.created_at)
 
-	return `<span class="ts">${d
-		.toISOString()
-		.replaceAll(/[a-z]+/gi, ' ')
-		.trim()
-		.slice(0, -4)}</span>`
+	return h`<span title="${full}" class="ts">${pretty}</span>`
 }
 
 function getLiHeading(x) {
 	switch (x.type) {
 		case 'IssuesEvent': {
-			return `
+			return h`
 				👾
-				${escape(capitalize(x.payload.action))}
+				${capitalize(x.payload.action)}
 				issue
 				${link({
 					url: x.payload.issue.html_url,
@@ -67,17 +133,17 @@ function getLiHeading(x) {
 			`
 		}
 		case 'WatchEvent': {
-			return `
+			return h`
 				👀
-				${escape(capitalize(x.payload.action))}
+				${capitalize(x.payload.action)}
 				watching
 				${repoLink(x, { mainLink: true })}
 			`
 		}
 		case 'PullRequestEvent': {
-			return `
+			return h`
 				⤴️
-				${escape(capitalize(x.payload.action))}
+				${capitalize(x.payload.action)}
 				pull request
 				${link({
 					url: x.payload.pull_request.html_url,
@@ -88,9 +154,9 @@ function getLiHeading(x) {
 			`
 		}
 		case 'IssueCommentEvent': {
-			return `
+			return h`
 				💬
-				${escape(capitalize(x.payload.action))}
+				${capitalize(x.payload.action)}
 				an
 				${link({
 					url: x.payload.comment.html_url,
@@ -101,7 +167,7 @@ function getLiHeading(x) {
 			`
 		}
 		case 'PushEvent': {
-			return `
+			return h`
 				➡️
 				Pushed commits
 				to
@@ -111,18 +177,18 @@ function getLiHeading(x) {
 		case 'CreateEvent': {
 			switch (x.payload.ref_type) {
 				case 'repository': {
-					return `
+					return h`
 						🆕
 						Created repository
 						${repoLink(x, { mainLink: true })}
 					`
 				}
 				default: {
-					return `
+					return h`
 						🆕
 						Created
-						${escape(x.payload.ref_type)}
-						<code>${escape(x.payload.ref)}</code>
+						${x.payload.ref_type}
+						<code>${x.payload.ref}</code>
 						on
 						${repoLink(x, { mainLink: true })}
 					`
@@ -130,7 +196,7 @@ function getLiHeading(x) {
 			}
 		}
 		case 'ForkEvent': {
-			return `
+			return h`
 				🍴
 				Forked
 				${repoLink(x)}
@@ -142,19 +208,19 @@ function getLiHeading(x) {
 			`
 		}
 		case 'DeleteEvent': {
-			return `
+			return h`
 				⛔
 				Deleted
-				${escape(x.payload.ref_type)}
-				<code>${escape(x.payload.ref)}</code>
+				${x.payload.ref_type}
+				<code>${x.payload.ref}</code>
 				on
 				${repoLink(x, { mainLink: true })}
 			`
 		}
 		case 'PullRequestReviewCommentEvent': {
-			return `
+			return h`
 				💬
-				${escape(capitalize(x.payload.action))}
+				${capitalize(x.payload.action)}
 				a
 				${link({
 					url: x.payload.comment.html_url,
@@ -165,9 +231,9 @@ function getLiHeading(x) {
 			`
 		}
 		case 'PullRequestReviewEvent': {
-			return `
+			return h`
 				💬
-				${escape(capitalize(x.payload.action))}
+				${capitalize(x.payload.action)}
 				a
 				${link({
 					url: x.payload.review.html_url,
@@ -177,36 +243,54 @@ function getLiHeading(x) {
 				${repoLink(x)}
 			`
 		}
+		case 'ReleaseEvent': {
+			return h`
+				🎉
+				${capitalize(x.payload.action)}
+				a
+				${link({
+					url: x.payload.release.html_url,
+					text: 'release',
+				})}
+				for tag
+				<code>${x.payload.release.tag_name}</code>
+				on
+				${repoLink(x)}
+			`
+		}
 		default: {
-			return `${x.type}`
+			return h`
+				❓
+				${sentenceCase(x.type)}
+				on
+				${repoLink(x, { mainLink: true })}
+			`
 		}
 	}
 }
 
 function render({ username, activities }) {
-	const html = `<div class="activities">
+	const html = h`<div class="activities">
 		<h1>${username}’s GitHub Activity Log</h1>
 		<ul>
-		${activities
-			.map((x) => {
-				let liHeading
+		${activities.map((x) => {
+			let liHeading
 
-				try {
-					liHeading = getLiHeading(x)
-				} catch (e) {
-					liHeading = `${x.type} ${e.name}: ${e.message}`
-				}
+			try {
+				liHeading = getLiHeading(x)
+			} catch (e) {
+				liHeading = `${x.type} ${e.name}: ${e.message}`
+			}
 
-				return `<li class="activity">
+			return h`<li class="activity">
 					<details>
 						<summary>
 							${liHeading} ${ts(x)}
 						</summary>
-						<pre>${escape(JSON.stringify(x, null, '\t'))}</pre>
+						<pre>${JSON.stringify(x, null, '\t')}</pre>
 					</details>
 				</li>`
-			})
-			.join('\n')}
+		})}
 		</ul>
 	</div>`
 
